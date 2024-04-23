c9322e94432321a4d4f98eda8e35c7007b5601e7
RafaMiura <143235499+RafaMiura@users.noreply.github.com>
2024-04-23 09:42:28 -0300

    Add files via upload

---
+++ assunto5_FilasEncadeadas/src/Filas/MainTeste.java
@@ -0,0 +1,18 @@
+package Filas;
+
+public class MainTeste {
+
+	public static void main(String[] args) {
+		FilaInt fila = new FilaInt();
+		fila.init();
+		
+		fila.enqueue(23);
+		fila.enqueue(44);
+		fila.enqueue(38);
+		
+		System.out.println(fila.dequeue());
+		System.out.println(fila.dequeue());
+		System.out.println(fila.dequeue());
+	}
+
+}
