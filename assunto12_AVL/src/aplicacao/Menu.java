06c07394d2173626d7befcfdacb2be377e1a3e3b
RafaMiura <143235499+RafaMiura@users.noreply.github.com>
2024-10-14 10:51:41 -0300

    Add files via upload

--- assunto12_AVL/src/aplicacao/Menu.java
+++ assunto12_AVL/src/aplicacao/Menu.java
@@ -5,32 +5,44 @@ import java.util.Scanner;
 import arvores.AVLINT;
 
 public class Menu {
+
 	public static void main(String[] args) {
-		Scanner entrada = new Scanner (System.in);
+		Scanner le = new Scanner(System.in);
 		AVLINT avl = new AVLINT();
 		int opcao;
 		do {
 			System.out.println("0 - Sair");
 			System.out.println("1 - Inserir");
-			System.out.println("2 - Apresenta a FB de cada no da AVL");
-			opcao = entrada.nextInt();
-		
-			switch(opcao) {
+			System.out.println("2 - Apresenta FB de cada no da AVL");
+			System.out.println("3 - Remover um valor da AVL");
+			opcao = le.nextInt();
+			switch (opcao) {
 			case 0:
 				System.out.println("Encerrado o programa");
 				break;
 			case 1:
-				System.out.println("Informa o valor do dado a ser inserido: ");
-				int valor = entrada.nextInt();
-				avl.root = avl.inserirH(avl.root, valor);
+				System.out.print("Informa valor do dado a ser inserido: ");
+				int valor = le.nextInt();
+				avl.root = avl.inserirAVL(avl.root, valor);
+				avl.atualizaAlturas(avl.root);
 				break;
-			case 2: 
-				System.out.println("/n*** Apresentacao de FB dos nos AVL");
+			case 2:
+				System.out.println("\n*** Apresentacao de FB dos nos AVL");
 				avl.mostraFB(avl.root);
+				break;	
+			case 3:
+				System.out.print("Informa valor do dado a ser removido: ");
+				valor = le.nextInt();
+				avl.root = avl.removeValor(avl.root, valor);
+				avl.root = avl.atualizaAlturaBalanceamento(avl.root);
+				avl.atualizaAlturas(avl.root);
 				break;
 			default:
 				System.out.println("Opcao invalida");
 			}
 		} while (opcao != 0);
+		le.close();
+
 	}
+
 }
